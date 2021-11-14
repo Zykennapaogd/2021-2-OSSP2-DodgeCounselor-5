e763b1df93e8bcfc1528acfceaf9a19305eb44eb
GoodleaF <85035312+GoodleaF@users.noreply.github.com>
2021-11-14 21:20:25 +0900

    Add files via upload

---
+++ dodgecallgg/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dodgecallgg.users.models import User
+from dodgecallgg.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
