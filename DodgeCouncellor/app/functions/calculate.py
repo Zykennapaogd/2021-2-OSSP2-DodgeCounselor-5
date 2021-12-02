17fcd008e73601a6f5bad1f487644916e7a87da0
hojong <33508056+LteFroggy@users.noreply.github.com>
2021-12-02 11:26:23 +0900

    출력부 수정 및 주석 추가

--- DodgeCouncellor/app/functions/calculate.py
+++ DodgeCouncellor/app/functions/calculate.py
@@ -15,7 +15,6 @@ def calculateScorePerUser(userName) :
     summonerDTO = fun.getSummonerInfo(userName)
     matchList = fun.getMatchBySummonerDTO(summonerDTO, 20)
     matchInfos = fun.getMatchInfoByMatchID(matchList)
-    print("호출 끝")
 
     resultSet = {
         "deathKingCount" : 0,
@@ -28,36 +27,39 @@ def calculateScorePerUser(userName) :
     }
 
     for i in range(len(matchInfos)) :
-        print((i+1), "번째 판 ")
         #처음에 0점짜리 추가
         resultSet['trollScore'].append(0)
 
         userLoc = fun.getUserLoc(matchInfos[i], summonerDTO['name'])
 
-        #DeathKing의 가중치는 5점
+        #DeathKing 구하는 부분
         if (fun.DeathKing(matchInfos[i], userLoc)) :
             resultSet['trollScore'][i] += DeathKingScore
             resultSet['deathKingCount'] += 1
 
-        #noItem의 가중치는 3점
+        #noItem 구하는 부분
         if (fun.buySameItems(matchInfos[i], userLoc)) :
             resultSet['trollScore'][i] += NoItemScore
             resultSet['badItemCount'] += 1
 
+        #badSpell 구하는 부분
         if (not(fun.UseCorrectSpell(matchInfos[i], userLoc))) :
             resultSet['trollScore'][i] += BadSpellScore
             resultSet['badSpellCount'] += 1
         
+        #딜량 차이 구하는 부분
         damageDiff = fun.damageDiffByPosition(matchInfos[i], userLoc)
         if (damageDiff != 0) :
             resultSet['trollScore'][i] += (damageDiff * DamageDiffWeight)
             resultSet['weakDamageCount'] += 1
 
+        #골드 차이 구하는 부분
         goldDiff = fun.goldDiffByPostion(matchInfos[i], userLoc)
         if (goldDiff != 0) :
             resultSet['trollScore'][i] += (goldDiff * GoldDiffWeight)
             resultSet['lackGoldCount'] += 1
 
+        #시야점수 차이 구하는 부분
         visionDiff = fun.visionScoreDiffByPosition(matchInfos[i], userLoc)
         if (visionDiff != 0) :
             resultSet['trollScore'][i] += (visionDiff * visionScoreWeight)
